7e96ec01e0369c09b5a2f95f6a3c175cb2e0f80c
agraham <agraham0925@gmail.com>
2018-10-09 12:10:41 -0500

    when key is pressed, sound plays.

--- 01 - JavaScript Drum Kit/js/app.js
+++ 01 - JavaScript Drum Kit/js/app.js
@@ -1,5 +1,10 @@
 console.log("click click")
 
 window.addEventListener('keydown', function(e){
-	console.log(e.keyCode)
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+
+	if(!audio)return; //if key pressed doesn't have an audio element, stop function
+
+	audio.play()
+
 })
