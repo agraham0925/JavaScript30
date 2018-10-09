09f13b12666bcfccdab2728dcefa44b40c9cc52c
agraham <agraham0925@gmail.com>
2018-10-09 12:13:26 -0500

    when key pressed, sound plays and console log key div info

--- 01 - JavaScript Drum Kit/js/app.js
+++ 01 - JavaScript Drum Kit/js/app.js
@@ -2,9 +2,10 @@ console.log("click click")
 
 window.addEventListener('keydown', function(e){
 	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
 
 	if(!audio)return; //if key pressed doesn't have an audio element, stop function
-
+	audio.currentTime = 0; //rewind to start (allows you to press key multiple times quickly)
 	audio.play()
-
+	console.log(key)
 })
