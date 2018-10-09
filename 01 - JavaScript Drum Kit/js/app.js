c52fbff519b549201e57e1a66a72e39ff822a744
agraham <agraham0925@gmail.com>
2018-10-09 12:41:25 -0500

    created separate function for playSound and move eventListener

--- 01 - JavaScript Drum Kit/js/app.js
+++ 01 - JavaScript Drum Kit/js/app.js
@@ -1,13 +1,24 @@
 console.log("click click")
 
-window.addEventListener('keydown', function(e){
+function playSound(e) {
 	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
 	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
 
-	if(!audio)return; //if key pressed doesn't have an audio element, stop function
+	if(!audio) return; //if key pressed doesn't have an audio element, stop function
 	audio.currentTime = 0; //rewind to start (allows you to press key multiple times quickly)
-	audio.play()
+	audio.play();
 
 	//adds highlight border to key
 	key.classList.add('playing');
-})
+}
+
+function removeTransition(e) {
+	if(e.propertyName !== 'transform') return; //skip it if it's not a transform
+
+	this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+window.addEventListener('keydown', playSound)
