ebb63add1bfc572e05deb4dd597e186e9c9537e1
agraham <agraham0925@gmail.com>
2018-10-12 10:59:11 -0500

    add blur functionality and have mvp

--- 03 - CSS Variables/js/app.js
+++ 03 - CSS Variables/js/app.js
@@ -5,7 +5,11 @@ const inputs = document.querySelectorAll('.controls input');
 
 
 function handleUpdate() {
-	console.log(this.value)
+	// to handle adding px for spacing and blur
+	const suffix = this.dataset.sizing || '';
+
+	document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
 
 inputs.forEach(input => input.addEventListener('change', handleUpdate))
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate))
