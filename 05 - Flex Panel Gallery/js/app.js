f6569d0bd81521e2242fe413f0b766bafcc069ca
agraham <agraham0925@gmail.com>
2018-10-18 22:14:31 -0500

    05 - add toggle function in js to expand panel when clicked

--- 05 - Flex Panel Gallery/js/app.js
+++ 05 - Flex Panel Gallery/js/app.js
@@ -1,1 +1,9 @@
 console.log("hello, is it me you're looking for?")
+
+const panels = document.querySelectorAll('.panel');
+
+function toggleOpen() {
+	this.classList.toggle('open');
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
