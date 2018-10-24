0cad829d3e2dceb9a129e6eef3eb7df1b2296b6d
agraham <agraham0925@gmail.com>
2018-10-24 16:55:25 -0500

    08 - complete exercise

---
+++ 08 - Fun with HTML5 Canvas/app/app.js
@@ -0,0 +1,57 @@
+console.log("hello, it's me")
+
+const canvas = document.querySelector('#draw')
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+ctx.strokeStyle = '#BADASS';
+// can go on mdn and look at all the different options for lineCap and lineJoin
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = 100;
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+let hue = 0;
+let direction = true;
+
+
+function draw(e){
+	if(!isDrawing) return; //this will stop the function from running when not moused down
+
+	console.log(e);
+	ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+	ctx.beginPath();
+	// start from
+	ctx.moveTo(lastX, lastY);
+	// go to
+	ctx.lineTo(e.offsetX, e.offsetY);
+	ctx.stroke();
+	lastX =  e.offsetX;
+	lastY = e.offsetY;
+
+	hue++;
+
+	if(ctx.lineWidth >= 100 || ctx.lineWidth <= 1){
+		direction = !direction;
+	}
+
+	if(direction){
+		ctx.lineWidth++;
+	} else {
+		ctx.lineWidth--;
+	}
+}
+
+canvas.addEventListener('mousedown', (e) => {
+	isDrawing = true;
+	lastX =  e.offsetX;
+	lastY = e.offsetY;
+})
+
+canvas.addEventListener('mousemove', draw)
+canvas.addEventListener('mouseup', () => isDrawing = false)
+canvas.addEventListener('mouseout', () => isDrawing = false)
